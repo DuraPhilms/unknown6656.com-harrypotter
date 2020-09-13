11c991f2a63406fc3a8ba092c99cdc1d06f83745
unknown6656 <unknown84@gmx.de>
2020-09-13 16:50:42 +0200

    overhauled most parts of the site

--- js/script.js
+++ js/script.js
@@ -1,134 +1,47 @@
-var urlbase = "https://dl.dropboxusercontent.com/"; // "//www.dailymotion.com/embed/video/";
-var urlparams = ".mp4?dl=0"; // "?api=true&autoplay=1";
 
-var pva_IDs = [
-    's/lxwndd282onxifp/Harry%20Potter%20und%20der%20Penner%20von%20Alcatraz%20Part%201', /* 's/p2xvo9tz6a5duvw/pva_1' */
-    's/o8ctiw27tqw06d5/Harry%20Potter%20und%20der%20Penner%20von%20Alcatraz%20Part%202', /* 'u/73253367/pva_2' */
-    's/gxv8gk0sx1cnsl8/Harry%20Potter%20und%20der%20Penner%20von%20Alcatraz%20Part%203', /* 'u/73253367/pva_3' */
-    's/10i1pbz8fauw6h8/Harry%20Potter%20und%20der%20Penner%20von%20Alcatraz%20Part%204', /* 'u/73253367/pva_4' */
-    's/lk5pkl09do8ze0x/Harry%20Potter%20und%20der%20Penner%20von%20Alcatraz%20Part%205', /* 'u/73253367/pva_5' */
-    's/ox6l2oh265prlsj/Harry%20Potter%20und%20der%20Penner%20von%20Alcatraz%20Part%206', /* 's/voem5t0895tvhjn/pva_6' */
-    's/3m3giez3ozzdyhp/Harry%20Potter%20und%20der%20Penner%20von%20Alcatraz%20Part%207', /* 's/20v8p10m2ltf4n7/pva_7' */
-    's/v12dtpns1ni90cc/Harry%20Potter%20und%20der%20Penner%20von%20Alcatraz%20Part%208', /* 's/g6ojq3d18cxv71z/pva_8' */
-    's/vczzrvvvya955t7/Harry%20Potter%20und%20der%20Penner%20von%20Alcatraz%20Part%209', /* 's/pd6owcjow3s3ef1/pva_9' */
-    's/co4hut6zix5ysmo/Harry%20Potter%20und%20der%20Penner%20von%20Alcatraz%20Part%2010', /* 's/6malrmv3dtjqm8c/pva_10' */
-    's/gnwjezggvl4pyap/Harry%20Potter%20und%20der%20Penner%20von%20Alcatraz%20Part%2011', /* 's/rxp2xf5opelis6x/pva_11' */
-    's/f90245uw35eq54d/Harry%20Potter%20und%20der%20Penner%20von%20Alcatraz%20Part%2012', /* 's/spjjp63035housv/pva_12' */
-    's/uk17eeco8lk2gg9/Harry%20Potter%20und%20der%20Penner%20von%20Alcatraz%20Part%2013', /* 's/faeb90rtul74kn4/pva_13' */
-    's/q3hfxu7mtd15f0b/Harry%20Potter%20und%20der%20Penner%20von%20Alcatraz%20Part%2014' /* 's/ch9jrf1p535mi3q/pva_14' */
-];
-var odp_IDs = [
-    's/aeidu1nx88ps38f/Harry%20Potter%20und%20der%20Orden%20des%20Penners%20Teil%201', /* 'u/73253367/odp_1' */
-    's/783cxbfiw3qbv9l/Harry%20Potter%20und%20der%20Orden%20des%20Penners%20Teil%202', /* 'u/73253367/odp_2' */
-    's/gq21qopqhbprsvn/Harry%20Potter%20und%20der%20Orden%20des%20Penners%20Teil%203', /* 'u/73253367/odp_3' */
-    's/47s3x5lrqniyh4i/Harry%20Potter%20und%20der%20Orden%20des%20Penners%20Teil%204', /* 'u/73253367/odp_4' */
-    's/zqva09o03qq6geu/Harry%20Potter%20und%20der%20Orden%20des%20Penners%20Teil%205', /* 'u/73253367/odp_5' */
-    's/fc2sjs4bek89a47/Harry%20Potter%20und%20der%20Orden%20des%20Penners%20Teil%206', /* 'u/73253367/odp_6' */
-    's/rr18znfqu0faibz/Harry%20Potter%20und%20der%20Orden%20des%20Penners%20Teil%207', /* 'u/73253367/odp_7' */
-    's/vnqhv9udkf9lutr/Harry%20Potter%20und%20der%20Orden%20des%20Penners%20Teil%208', /* 'u/73253367/odp_8' */
-    's/clpkj0vs52zdhn6/Harry%20Potter%20und%20der%20Orden%20des%20Penners%20Teil%209', /* 'u/73253367/odp_9' */
-    's/9kbbdgkn69mm49c/Harry%20Potter%20und%20der%20Orden%20des%20Penners%20Teil%2010', /* 'u/73253367/odp_10' */
-    's/p8mobdk1urh9nzn/Harry%20Potter%20und%20der%20Orden%20des%20Penners%20Teil%2011', /* 'u/73253367/odp_11' */
-    's/mdjhpaw43xg77n7/Harry%20Potter%20und%20der%20Orden%20des%20Penners%20Teil%2012',
-    ''
-];
-var hlp_IDs = [
-    ''
-];
+$('#selector-part').on('change', function()
+{
+    let selected = this.value;
+    let video = $('video#player source')
 
-$('#sp_pva').attr('max', pva_IDs.length);
-$('#sp_pva').attr('placeholder', '1 ... ' + pva_IDs.length);
-$('#pva_lim').html(pva_IDs.length);
+    if (selected.length < 1)
+        video.attr('src', '');
+    else
+    {
+        let parts = selected.split('-');
 
-$('#sp_odp').attr('max', odp_IDs.length);
-$('#sp_odp').attr('placeholder', '1 ... ' + odp_IDs.length);
-$('#odp_lim').html(odp_IDs.length);
+        if (parts[1].length == 1)
+            parts[1] = '0' + parts[1];
 
-$('#sp_hlp').attr('max', hlp_IDs.length);
-$('#sp_hlp').attr('placeholder', '1 ... ' + hlp_IDs.length);
-$('#hlp_lim').html(hlp_IDs.length);
-
-// var offline_string = "";
-
-var pva_id_str = 'table#parts tr td#pva';
-var odp_id_str = 'table#parts tr td#odp';
-var hlp_id_str = 'table#parts tr td#hlp';
-
-for (var i = 0; i < pva_IDs.length; i++) {
-    if (pva_IDs[i] == '') {
-        $(pva_id_str + (i + 1)).addClass('offline');
-        $(pva_id_str + (i + 1) + ' span.tooltip').attr('text',
-            'HPudPvA Part ' + (i + 1) + ' ist leider (noch) nicht online');
-    } else if (false) {
-        // CROSS-DOMAIN SYNCHRON PING
-
-        $(pva_id_str + (i + 1)).addClass('temporary');
-        $(pva_id_str + (i + 1) + ' span.tooltip').attr('text',
-            'HPudPvA Part ' + (i + 1) + ' ist zurzeit nicht erreichbar');
-    } else {
-        $(pva_id_str + (i + 1)).addClass('online');
-        $(pva_id_str + (i + 1) + ' span.tooltip').attr('text',
-            'HPudPvA Part ' + (i + 1) + ' ist online');
+        video.attr('src', 'videos/' + parts[0] + '/' + parts[1] + '.mp4');
     }
-    // if (pva_IDs[i] == '')
-    //	   offline_string += "<br/>&#160;&#160;&#160; - HPudPvA Teil " + (i + 1);
-}
-
-for (var i = 0; i < odp_IDs.length; i++) {
-    if (odp_IDs[i] == '') {
-        $(odp_id_str + (i + 1)).addClass('offline');
-        $(odp_id_str + (i + 1) + ' span.tooltip').attr('text',
-            'HPudOdP Part ' + (i + 1) + ' ist leider (noch) nicht online');
-    } else if (false) {
-        // CROSS-DOMAIN SYNCHRON PING
-
-        $(odp_id_str + (i + 1)).addClass('temporary');
-        $(odp_id_str + (i + 1) + ' span.tooltip').attr('text',
-            'HPudOdP Part ' + (i + 1) + ' ist zurzeit nicht erreichbar');
-    } else {
-        $(odp_id_str + (i + 1)).addClass('online');
-        $(odp_id_str + (i + 1) + ' span.tooltip').attr('text',
-            'HPudOdP Part ' + (i + 1) + ' ist online');
-    }
-    // if (odp_IDs[i] == '')
-    //     offline_string += "<br/>&#160;&#160;&#160; - HPudOdP Teil " + (i + 1);
-}
-
-for (var i = 0; i < hlp_IDs.length; i++) {
-    if (hlp_IDs[i] == '') {
-        $(hlp_id_str + (i + 1)).addClass('offline');
-        $(hlp_id_str + (i + 1) + ' span.tooltip').attr('text',
-            'HPudHLP Part ' + (i + 1) + ' ist leider (noch) nicht online');
-    } else if (false) {
-        // CROSS-DOMAIN SYNCHRON PING
-
-        $(hlp_id_str + (i + 1)).addClass('temporary');
-        $(hlp_id_str + (i + 1) + ' span.tooltip').attr('text',
-            'HPudHLP Part ' + (i + 1) + ' ist zurzeit nicht erreichbar');
-    } else {
-        $(hlp_id_str + (i + 1)).addClass('online');
-        $(hlp_id_str + (i + 1) + ' span.tooltip').attr('text',
-            'HPudHLP Part ' + (i + 1) + ' ist online');
-    }
-    // if (hlp_IDs[i] == '')
-    //     offline_string += "<br/>&#160;&#160;&#160; - HPudHLP Teil " + (i + 1);
-}
-
-// offline_string += "<br/>Einige andere Teile wurden aus Copyright-gründen wieder gelöscht - ich schaue mal in den kommenden Tagen nach einer guten Plattform";
-// $('span#offline').html(offline_string);
+});
 
 
-//-------- PROTOTYPE FUNCTIONS --------//
 
-if (typeof String.prototype.startsWith != 'function') {
-    String.prototype.startsWith = function(str) {
-        return this.indexOf(str) === 0;
-    };
-}
+video_ids.forEach(function (element)
+{
+    let table_id = 'td#' + element[0] + element[1];
+
+    $.ajax({
+        url : 'videos/' + element[0] + '/' + element[1] + '.mp4',
+        type : 'HEAD',
+        success : function()
+        {
+            $(table_id).addClass('online');
+            $(table_id + ' span.tooltip').attr('text', element[2] + '\nPart ' + element[1] + ' ist online');
+        },
+        error : function()
+        {
+            $(table_id).addClass('offline');
+            $(table_id + ' span.tooltip').attr('text', element[2] + '\nPart ' + element[1] + ' ist leider (noch) nicht online');
+        }
+    });
+});
+
 
 //-------- JQUERY HANDLERS --------//
 
-//$('title').html($('div#title').html());
 $('video').click(function(event) {
     var tag = $(event.target).get(0);
 
@@ -136,33 +49,6 @@ $('video').click(function(event) {
         tag.play();
     else
         tag.pause();
-});
-$('#sp_pva').change(OKDchg_pva);
-$('#sp_pva').keydown(function(event) {
-    if (event.keyCode == 13)
-    {
-        OKDchg_pva();
-
-        return false;
-    }
-});
-$('#sp_odp').change(OKDchg_odp);
-$('#sp_odp').keydown(function(event) {
-    if (event.keyCode == 13)
-    {
-        OKDchg_odp();
-
-        return false;
-    }
-});
-$('#sp_hlp').change(OKDchg_hlp);
-$('#sp_hlp').keydown(function(event) {
-    if (event.keyCode == 13)
-    {
-        OKDchg_hlp();
-
-        return false;
-    }
 });
 $('a[href*=#]:not([href=#])').click(function() {
     if ((location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')) ||
